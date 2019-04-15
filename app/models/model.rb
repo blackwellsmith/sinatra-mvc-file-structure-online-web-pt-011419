d2e41e45f4197485d12e8df6065d22f13339e643
blackwell <blackwellsmithiv@gmail.com>
2019-04-15 11:35:51 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
+class Dog 
+  attr_reader :breed 
+  attr_accessor :age, :name 
+  
+  @@dogs = []
+  
+  def initialize(breed, age, name)
+    @breed = breed
+    @age = age
+    @name = name
+    @@dogs << self 
+  end 
+  
+  
 
 end
